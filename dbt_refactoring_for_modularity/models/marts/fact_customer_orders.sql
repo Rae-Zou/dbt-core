6e7c6d60da4ec44e48038c28d648d61868414a46
rae <jkraezou@gmail.com>
2024-01-06 15:17:02 +1300

    more CTEs

--- dbt_refactoring_for_modularity/models/marts/fact_customer_orders.sql
+++ dbt_refactoring_for_modularity/models/marts/fact_customer_orders.sql
@@ -7,71 +7,72 @@ orders as (
 customers as (
   select * from {{ ref('stg_main_jaffle_shop__customers') }}
 ),
-
 -- Logical CTEs
-customer_order_history as (
-  select 
-      customers.customer_id,
-      customers.full_name,
-      customers.surname,
-      customers.givenname,
-      min(orders.order_date) as first_order_date,
-      min(orders.valid_order_date) as first_non_returned_order_date,
-      max(orders.valid_order_date) as most_recent_non_returned_order_date,
-      COALESCE(max(orders.user_order_seq),0) as order_count,
-      COALESCE(count(case 
-                    when orders.valid_order_date is not null 
-                    then 1 
-                    end),0) as non_returned_order_count,
-      sum(case 
+customer_orders as (
+  select
+    orders.*,
+    customers.full_name,
+    customers.surname,
+    customers.givenname,
+    --- customer level aggregations
+    min(orders.order_date) over(
+      partition by orders.customer_id
+    ) as customers_first_order_date,
+
+    min(orders.valid_order_date) over(
+      partition by orders.customer_id
+    ) as customers_first_non_returned_order_date,
+
+    max(orders.valid_order_date) over(
+      partition by orders.customer_id
+    ) as customers_most_recent_non_returned_order_date,
+
+    count(*) over (
+      partition by orders.customer_id
+    ) as customers_order_count,
+
+    sum(case 
+        when orders.valid_order_date is not null 
+        then 1 else 0
+        end) over (
+      partition by orders.customer_id
+    ) as customers_non_returned_order_count,
+
+    sum(case 
         when orders.valid_order_date is not null  
         then orders.order_value_dollars else 0 
-        end) as total_lifetime_value,
-      sum(case 
-        when orders.valid_order_date is not null  
-        then orders.order_value_dollars else 0  
-        end)/NULLIF(count(case 
-                          when orders.valid_order_date is not null
-                          then 1 
-                          end),0) as avg_non_returned_order_value,
-      array_agg(distinct orders.order_id) as order_ids
+        end) over (
+      partition by orders.customer_id
+    ) as customers_total_lifetime_value,
+    
+    array_agg(distinct orders.order_id) as customers_order_ids
 
   from orders
+  inner join customers
+    on orders.customer_id = customers.customer_id
+  group by 1,2,3,4,5,6,7,8,9,10
 
-  join customers
-  on orders.customer_id = customers.customer_id
-
-  {# left outer join payments
-  on orders.order_id = payments.order_id #}
-
-  -- where orders.order_status NOT IN ('pending') -- pending does not exit in orders table, it can be removed 
-
-  group by customers.customer_id, customers.full_name, customers.surname, customers.givenname
-
+),
+---
+add_avg_order_values as(
+  select
+    *,
+    customers_total_lifetime_value / customers_non_returned_order_count as customers_avg_non_returned_order_value
+  from customer_orders
 ),
 -- Final CTE
 final as (
   select 
-    orders.order_id,
-    orders.customer_id,
-    customers.surname,
-    customers.givenname,
-    first_order_date,
-    order_count,
-    total_lifetime_value,
-    orders.order_value_dollars,
-    orders.order_status
-  from orders 
-
-  join customers
-  on orders.customer_id = customers.customer_id
-
-  join customer_order_history
-  on orders.customer_id = customer_order_history.customer_id
-
-  {# left outer join payments
-  on orders.order_id = payments.order_id #}
-
+    order_id,
+    customer_id,
+    surname,
+    givenname,
+    customers_first_order_date as first_order_date,
+    customers_order_count as order_count,
+    customers_total_lifetime_value as total_lifetime_value,
+    order_value_dollars,
+    order_status
+  from add_avg_order_values 
   
 )
 -- Simple select statement
