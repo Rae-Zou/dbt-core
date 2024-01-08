0cece986f4ca41112a7b502deff61abfdaf11620
rae <jkraezou@gmail.com>
2024-01-08 20:06:02 +1300

    use windows functions

--- dbt_refactoring_practice/models/marts/fct_customer_orders.sql
+++ dbt_refactoring_practice/models/marts/fct_customer_orders.sql
@@ -43,35 +43,30 @@ final as (
     select
         paid_orders.*,
         -- sales transaction sequence
-        ROW_NUMBER() OVER (ORDER BY p.order_id) as transaction_seq,
+        ROW_NUMBER() OVER (ORDER BY paid_orders.order_id) as transaction_seq,
         -- customer sales sequence
-        ROW_NUMBER() OVER (PARTITION BY customer_id ORDER BY p.order_id) as customer_sales_seq,
+        ROW_NUMBER() OVER (PARTITION BY customer_id ORDER BY paid_orders.order_id) as customer_sales_seq,
         -- new v.s. returning customers
-        CASE 
-            WHEN c.first_order_date = p.order_placed_at
-            THEN 'new'
-            ELSE 'return' 
-        END as nvsr,
+        case  
+            when (
+                rank() over (
+                partition by paid_orders.customer_id
+                order by paid_orders.order_placed_at, paid_orders.order_id
+                ) = 1
+            ) then 'new'
+            else 'return' 
+        end as nvsr,
         -- customer lifetime value
-        x.clv_bad as customer_lifetime_value,
-        -- 1st day of sale
-        first_value(order_placed_at) over (
+         sum(total_amount_paid) over (
             partition by customer_id
             order by order_placed_at
+            ) as customer_lifetime_value,
+        -- 1st day of sale
+        first_value(paid_orders.order_placed_at) over (
+            partition by customer_id
+            order by paid_orders.order_placed_at
             ) as fdos
     FROM paid_orders
-    left join customer_orders USING (customer_id)
-    LEFT OUTER JOIN 
-    (
-        select
-            paid_orders.order_id,
-            sum(t2.total_amount_paid) as clv_bad
-        from paid_orders
-        left join paid_orders t2 
-            on paid_orders.customer_id = t2.customer_id and paid_orders.order_id >= t2.order_id
-        group by 1
-        order by paid_orders.order_id
-    ) x on x.order_id = paid_orders.order_id
     ORDER BY paid_orders.order_id
 )
 
